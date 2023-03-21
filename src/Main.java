62291e56ad55123622a2d68397738cc943f0db8f
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-21 07:38:28 +0400

    Correct Task

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         Book warAndPeace = new Book("Война и Мир", tolstoy, 1865);
         Book masterAndMargarita = new Book("Мастер и маргарита", bulgakov, 1966);
         System.out.println("Книга 1. Автор-" + tolstoy.getAuthorName() + ". Название-" + warAndPeace.getBookTitle() + ". Год публикации-" + warAndPeace.getPublishingYear());
-       /* System.out.println("Книга 2. Автор-" + masterAndMargarita.getAuthor() + ". Название-" + masterAndMargarita.getBookTitle() + ". Год публикации-" + masterAndMargarita.getPublishingYear());
+       System.out.println("Книга 2. Автор-" + bulgakov.getAuthorName() + ". Название-" + masterAndMargarita.getBookTitle() + ". Год публикации-" + masterAndMargarita.getPublishingYear());
         masterAndMargarita.setPublishingYear(1967);
-        System.out.println("Книга 3. Автор-" + masterAndMargarita.getAuthor() + ". Название-" + masterAndMargarita.getBookTitle() + ". Год публикации-" + masterAndMargarita.getPublishingYear());
-*/
+        System.out.println("Книга 3. Автор-" + bulgakov.getAuthorName() + ". Название-" + masterAndMargarita.getBookTitle() + ". Год публикации-" + masterAndMargarita.getPublishingYear());
+
     }
 }
